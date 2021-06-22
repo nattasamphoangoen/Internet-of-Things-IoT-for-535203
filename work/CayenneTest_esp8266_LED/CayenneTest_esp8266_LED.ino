e760e741ad4af694142acccd39747ff61dc17a53
nattasamphoangoen <natta151515@gmail.com>
2021-06-22 14:20:21 +0700

    update TestEsp8266Uno

--- work/CayenneTest_esp8266_LED/CayenneTest_esp8266_LED.ino
+++ work/CayenneTest_esp8266_LED/CayenneTest_esp8266_LED.ino
@@ -9,8 +9,9 @@ char password[] = "26049979";
 char username[] = "c753c300-ea41-11e9-a38a-d57172a4b4d4";
 char mqtt_password[] = "2acffd0bf32ae3ce0340aa228216051a8ff522f3";
 char cliend_id[] = "255c2410-d2f8-11eb-b767-3f1a8f1211ba";
-#define RT0 10000  
-#define B 3977     
+
+#define RT0 10000
+#define B 3977
 float a = A0;
 #define VCC 5
 #define R 10000
@@ -33,32 +34,34 @@ void setup() {
 
 
 void loop() {
-  
-  Cayenne.loop();
-  
-}
 
+  Cayenne.loop();
+
+}
+int Temp_F, Temp_K;
 CAYENNE_OUT_DEFAULT()
 {
   //thermistor 10k
-  Read = analogRead(A0);             
-  Read = (5.00 / 1023.00) * Read;     
+  Read = analogRead(A0);
+  Read = (5.00 / 1023.00) * Read;
   VR = VCC - Read;
-  RT = Read / (VR / R);              
+  RT = Read / (VR / R);
   ln = log(RT / RT0);
-  Temp = (1 / ((ln / B) + (1 / T0))); 
-  Temp = Temp - 273.15;                
-
+  Temp = (1 / ((ln / B) + (1 / T0)));
+  Temp = Temp - 273.15;
+  Temp_F = (Temp * 1.8) + 32;
+  Temp_K = Temp + 273.15;
   Serial.print("Temperature:");
   Serial.print("\t");
-  Serial.print(Temp);       
+  Serial.print(Temp);
   Serial.print("C\t\t");
-  Serial.print(Temp + 273.15);        
+  Serial.print(Temp + 273.15);
   Serial.print("K\t\t");
-  Serial.print((Temp * 1.8) + 32);    
+  Serial.print((Temp * 1.8) + 32);
   Serial.println("F");
   Cayenne.virtualWrite(3, String(Temp).c_str());
-
+  Cayenne.virtualWrite(4, String(Temp_F).c_str());
+  Cayenne.virtualWrite(5, String(Temp_K).c_str());
   //DHT11
   float humidity = dht.getHumidity();
   float temperature = dht.getTemperature();
@@ -68,8 +71,9 @@ CAYENNE_OUT_DEFAULT()
   Serial.println("\t\t");
   Cayenne.virtualWrite(2, String(humidity).c_str());
   Cayenne.virtualWrite(1, String(temperature).c_str());
-  
-  delay(1000);
+  Cayenne.virtualWrite(6, String(temperature + 273.15).c_str());
+  Cayenne.virtualWrite(7, String((temperature * 1.8) + 32).c_str());
+
 }
 
 CAYENNE_IN_DEFAULT()
